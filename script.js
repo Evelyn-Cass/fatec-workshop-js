4e9edf243052ebf5084c6df078ec6a778bf10d03
Evelyn Cassinotte <cassinotte@gmail.com>
2024-09-05 19:59:18 -0300

    modified: correcao observacoes

--- script.js
+++ script.js
@@ -22,9 +22,10 @@ A linha 9 está incorreta porque, para declarar duas constantes, é necessário 
 de atribuição para ambas (x e y), ou então declarar a segunda constante em uma linha separada. 
 Exemplos corretos seriam: {const x = 5, y = 10;} ou {const x = 5; const y = 10;}.
 
-Na linha 10, há uma tentativa de modificar o valor de x, mas como x foi declarado como uma constante,
-seu valor não pode ser alterado após a atribuição inicial. Para corrigir isso, 
-x deve ser declarado como uma variável, utilizando let: {let x = 5;}.
+Na linha 10, vemos o código {x + 10}, o que indica uma tentativa de somar 10 ao valor de x. 
+A forma correta de fazer isso seria {x = x + 10} ou {x += 10}.
+No entanto, como x foi declarado como uma constante, seu valor não pode ser alterado após a atribuição inicial.
+Para corrigir isso, x deve ser declarado como uma variável, utilizando let: {let x = 5;}.
 
 Após essas mudanças, ao executar o código, o retorno de {console.log} é {undefined}.
 Isso acontece porque a função somar não contém um {return} explícito, 
